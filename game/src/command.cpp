c8cf3553f3c93e9be77d5d43229858a71318159a
sunm0710 <sunm0710@126.com>
2017-08-01 14:56:37 -0700

    modify command

--- game/src/command.cpp
+++ game/src/command.cpp
@@ -1,43 +1,44 @@
 #include "../headers/display.h"
 
 namespace command {
-	void plotAnima(Boxes& boxes) {
-		display::plotAnima(boxes);
-	}
+	
+}
+void command::plotAnima(Boxes& boxes) {
+	display::plotAnima(boxes);
+}
 
-	void choose(Boxes& boxes, int boxNo) {
-		display::choose(boxes, boxNo);
-	}
+void command::choose(Boxes& boxes, int boxNo) {
+	display::choose(boxes, boxNo);
+}
 
-	void gameStart(Boxes& boxes) {
-		display::gameStart(boxes);
-	}
+void command::gameStart(Boxes& boxes) {
+	display::gameStart(boxes);
+}
 
-	void gameEnd(Boxes& boxes) {
-		display::gameEnd(boxes);
-	}
+void command::gameEnd(Boxes& boxes) {
+	display::gameEnd(boxes);
+}
 
-	void gameOpen(Boxes& boxes, int choice) {
-		display::gameOpen(boxes, choice);
-	}
+void command::gameOpen(Boxes& boxes, int choice) {
+	display::gameOpen(boxes, choice);
+}
 
-	void rightOpen(Boxes& boxes, int choice) {
-		display::rightOpen(boxes, choice);
-	}
+void command::rightOpen(Boxes& boxes, int choice) {
+	display::rightOpen(boxes, choice);
+}
 
-	void wrongOpen(Boxes& boxes, int choice) {
-		display::wrongOpen(boxes, choice);
-	}
+void command::wrongOpen(Boxes& boxes, int choice) {
+	display::wrongOpen(boxes, choice);
+}
 
-	void ready(char const* info) {
-		display::ready(info);
-	}
+void command::ready(char const* info) {
+	display::ready(info);
+}
 
-	void settingStart() {
-		display::settingStart();
-	}
+void command::settingStart() {
+	display::settingStart();
+}
 
-	void settingMenu(char const* item, int value, int min, int max, int line) {
-		display::settingMenu(item, value, min, max, line);
-	}
+void command::settingMenu(char const* item, int value, int min, int max, int line) {
+	display::settingMenu(item, value, min, max, line);
 }
