a459e638a9c35707201add84432ced94bb3b2581
sunm0710 <sunm0710@126.com>
2017-07-31 22:43:42 -0700

    add command

---
+++ game/src/command.cpp
@@ -0,0 +1,19 @@
+#include "../headers/display.h"
+
+namespace command {
+	void plotAnima(Boxes& boxes) {
+		display::plotAnima(boxes);
+	}
+
+	void choose(Boxes& boxes, int boxNo) {
+		display::choose(boxes, boxNo);
+	}
+
+	void gameStart(Boxes& boxes) {
+		display::gameStart(boxes);
+	}
+
+	void ready(char* info) {
+		display::ready(info);
+	}
+}
