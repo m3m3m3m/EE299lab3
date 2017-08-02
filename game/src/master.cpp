a4cbe51b9e8c9ba8ce18c9bb7e0b46d48a4c9090
sunm0710 <sunm0710@126.com>
2017-08-02 03:35:56 -0700

    ...

--- game/src/master.cpp
+++ game/src/master.cpp
@@ -2,10 +2,12 @@
 #include "Arduino.h"
 #include "../headers/game.h"
 #include "../headers/control.h"
+#include <time.h>
 
 void master::setup() {
 	Serial.begin(BAUD_RATE);
 	control::begin();
+	randomSeed((unsigned)time(NULL));
 	game::tutorial();
 }
 
